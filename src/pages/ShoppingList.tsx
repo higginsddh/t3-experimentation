fd9d1cf7003a1d191ccd72cc7f194e0e3d96d5e6
Daniel Higgins <daniel.higgins@gmail.com>
2022-11-20 12:49:22 -0500

    more shopping list change

--- src/pages/ShoppingList.tsx
+++ src/pages/ShoppingList.tsx
@@ -16,7 +16,7 @@ import {
   NumberInput,
   TextInput,
 } from "@mantine/core";
-import { IconPlus } from "@tabler/icons";
+import { IconArrowsMoveVertical, IconPlus } from "@tabler/icons";
 
 type ShoppingListItemValues = {
   text: string;
@@ -70,28 +70,21 @@ const ShoppingList: React.FC = () => {
       </form>
 
       {items?.map((i) => (
-        <div className="flex w-full" key={i.id}>
-          <input
-            type="text"
-            className="block flex-1 rounded border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="Add item..."
-            value={i.item}
-            required
-          />
-          <input
-            type="number"
-            className="ml-5 w-14 rounded border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            value={i.quantity.toString()}
-            required
-          />
-          <button
-            type="submit"
-            className="ml-5 inline-flex items-center rounded border border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400"
-            disabled
-          >
-            <FontAwesomeIcon icon={faArrowsUpDown} className="w-2" />
-          </button>
-        </div>
+        <ShoppingListItemForm
+          key={i.id}
+          icon={
+            <ActionIcon variant="filled" size={"lg"}>
+              <IconArrowsMoveVertical size={18} />
+            </ActionIcon>
+          }
+          values={{
+            text: i.item,
+            quantity: i.quantity,
+          }}
+          onValuesChange={() => {
+            return;
+          }}
+        />
       ))}
     </>
   );
@@ -109,7 +102,7 @@ function ShoppingListItemForm({
   onValuesChange(args: ShoppingListItemValues): void;
 }) {
   return (
-    <Flex columnGap={"md"}>
+    <Flex columnGap={"md"} mb="sm">
       <TextInput
         placeholder="Add item..."
         value={values.text}
