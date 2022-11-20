d2186bdf4d0c33d542057126240ee10d61c5524e
Daniel Higgins <daniel.higgins@gmail.com>
2022-11-20 17:25:52 -0500

    fix ordering

--- src/server/trpc/router/shoppingList.ts
+++ src/server/trpc/router/shoppingList.ts
@@ -2,11 +2,35 @@ import { z } from "zod";
 
 import { router, publicProcedure } from "../trpc";
 
+const shoppingListBase = z.object({
+  text: z.string(),
+  quantity: z.number(),
+  order: z.number().optional(),
+});
+
+const shoppingListUpdate = shoppingListBase
+  .extend({
+    id: z.string(),
+  })
+  .partial({
+    order: true,
+    quantity: true,
+    text: true,
+  });
+
 export const shoppingListRouter = router({
   addItem: publicProcedure
-    .input(z.object({ text: z.string(), quantity: z.number() }))
-    .mutation(({ input, ctx }) => {
-      return ctx.prisma.shoppingListItem.create({
+    .input(shoppingListBase)
+    .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.shoppingListItem.updateMany({
+        data: {
+          order: {
+            increment: 1,
+          },
+        },
+      });
+
+      return await ctx.prisma.shoppingListItem.create({
         data: {
           item: input.text,
           quantity: input.quantity,
@@ -14,7 +38,27 @@ export const shoppingListRouter = router({
         },
       });
     }),
+
+  updateItem: publicProcedure
+    .input(shoppingListUpdate)
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.shoppingListItem.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          ...input,
+        },
+      });
+    }),
+
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.shoppingListItem.findMany();
+    return ctx.prisma.shoppingListItem.findMany({
+      orderBy: [
+        {
+          order: "asc",
+        },
+      ],
+    });
   }),
 });
