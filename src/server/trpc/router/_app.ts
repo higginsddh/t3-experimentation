08cd1f4c6f21c1f26686f8ac5f997780ece18a8f
Daniel Higgins <daniel.higgins@gmail.com>
2022-11-20 15:00:41 -0500

    fix server side render

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { shoppingListRouter } from "./shoppingList";
 
 export const appRouter = router({
-  example: exampleRouter,
   shoppingList: shoppingListRouter,
   auth: authRouter,
 });
