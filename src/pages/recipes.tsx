0111835d5f02cd55f06b986595e62e7d556f4391
Daniel Higgins <daniel.higgins@gmail.com>
2023-03-18 11:31:36 -0400

    recipes

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -1,11 +1,43 @@
-import { Container } from "@mantine/core";
+import {
+  ActionIcon,
+  Badge,
+  Button,
+  Card,
+  Container,
+  Group,
+  Text,
+} from "@mantine/core";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
 import { type NextPage } from "next";
 
 const Recipes: NextPage = () => {
   return (
     <>
       <Container>
-        Recipes!
+        <Card withBorder>
+          <Group position="apart" mb="xs">
+            <Text weight={500}>Brown butter cod</Text>
+            <Group spacing="xs">
+              <ActionIcon>
+                <IconEdit />
+              </ActionIcon>
+              <ActionIcon>
+                <IconTrash />
+              </ActionIcon>
+            </Group>
+          </Group>
+          <Group>This is a recipe from Blue Apron.</Group>
+          <Group position="apart">
+            <Group>
+              <Badge color="red" variant="light">
+                Seafood
+              </Badge>
+            </Group>
+            <Button size="sm" variant="light">
+              Add to shopping list
+            </Button>
+          </Group>
+        </Card>
       </Container>
     </>
   );
