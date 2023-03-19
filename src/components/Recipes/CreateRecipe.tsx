bb27ae83ae549bc859719cd53a768b40770e050b
Daniel Higgins <daniel.higgins@gmail.com>
2023-03-19 15:04:18 -0400

    add form

--- src/components/Recipes/CreateRecipe.tsx
+++ src/components/Recipes/CreateRecipe.tsx
@@ -1,14 +1,36 @@
-import { Button, Group, Modal } from "@mantine/core";
+import { TextInput, Button, Group, Modal } from "@mantine/core";
+import { Select } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 export function CreateRecipe({ onClose }: { onClose: () => void }) {
+  const form = useForm<{ title: string; tags: Array<string> }>({
+    initialValues: {
+      title: "",
+      tags: [],
+    },
+  });
+
   return (
     <>
       <Modal opened={true} onClose={onClose} title="Add Recipe">
-        <Group></Group>
+        <form
+          onSubmit={form.onSubmit((v) => {
+            console.log(v);
+          })}
+        >
+          <TextInput
+            withAsterisk
+            label="Title"
+            width={"100%"}
+            {...form.getInputProps("title")}
+          />
 
-        <Group position="right">
-          <Button color="blue">Save</Button>
-        </Group>
+          <Group position="right" mt="md">
+            <Button color="blue" type="submit">
+              Save
+            </Button>
+          </Group>
+        </form>
       </Modal>
     </>
   );
