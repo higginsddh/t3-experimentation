a3a837775a96a7fdec1ad3589a94ddc5e24781c7
Daniel Higgins <daniel.higgins@gmail.com>
2024-11-14 15:08:20 -0500

    revert

--- src/components/ShoppingList/ShoppingListExistingItems.tsx
+++ src/components/ShoppingList/ShoppingListExistingItems.tsx
@@ -1,45 +1,45 @@
-import type { ShoppingListItem } from "@prisma/client";
 import { ShoppingListExistingItem } from "./ShoppingListItem";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import { trpc } from "../../utils/trpc";
 import { ShoppingListDeleteItems } from "./ShoppingListDeleteItems";
-import { getReorderedItems } from "./ShoppingListExistingItems.functions";
 import { NonBlockingLoader } from "../NonBlockingLoader";
+import type { ShoppingListItem } from "../../models/ShoppingListItem";
 
 export function ShoppingListExistingItems({
   items,
 }: {
   items: Array<ShoppingListItem>;
 }) {
-  const utils = trpc.useContext();
-  console.log(items);
+  // const utils = trpc.useContext();
+  // console.log(items);
 
-  const { mutate: reorder, isLoading } = trpc.shoppingList.reorder.useMutation({
-    async onMutate(args) {
-      // Cancel outgoing fetches (so they don't overwrite our optimistic update)
-      await utils.shoppingList.getAll.cancel();
+  // const { mutate: reorder, isLoading } = trpc.shoppingList.reorder.useMutation({
+  //   async onMutate(args) {
+  //     // Cancel outgoing fetches (so they don't overwrite our optimistic update)
+  //     await utils.shoppingList.getAll.cancel();
 
-      const previousData = utils.shoppingList.getAll.getData();
+  //     const previousData = utils.shoppingList.getAll.getData();
 
-      utils.shoppingList.getAll.setData(undefined, (old) => {
-        return getReorderedItems(args, old);
-      });
+  //     utils.shoppingList.getAll.setData(undefined, (old) => {
+  //       return getReorderedItems(args, old);
+  //     });
 
-      return { previousData };
-    },
+  //     return { previousData };
+  //   },
 
-    onSettled: () => {
-      utils.shoppingList.getAll.invalidate();
-    },
+  //   onSettled: () => {
+  //     utils.shoppingList.getAll.invalidate();
+  //   },
 
-    onError(err, newItem, ctx) {
-      // If the mutation fails, use the context-value from onMutate
-      utils.shoppingList.getAll.setData(
-        undefined,
-        () => ctx?.previousData ?? []
-      );
-    },
-  });
+  //   onError(err, newItem, ctx) {
+  //     // If the mutation fails, use the context-value from onMutate
+  //     utils.shoppingList.getAll.setData(
+  //       undefined,
+  //       () => ctx?.previousData ?? []
+  //     );
+  //   },
+  // });
+
+  const isLoading = false;
 
   return (
     <>
@@ -50,19 +50,19 @@ export function ShoppingListExistingItems({
               return;
             }
 
-            let precedingId: string | null;
-            if (result.destination.index === 0) {
-              precedingId = null;
-            } else if (result.source.index > result.destination.index) {
-              precedingId = items[result.destination.index - 1]?.id ?? null;
-            } else {
-              precedingId = items[result.destination.index]?.id ?? null;
-            }
+            // let precedingId: string | null;
+            // if (result.destination.index === 0) {
+            //   precedingId = null;
+            // } else if (result.source.index > result.destination.index) {
+            //   precedingId = items[result.destination.index - 1]?.id ?? null;
+            // } else {
+            //   precedingId = items[result.destination.index]?.id ?? null;
+            // }
 
-            reorder({
-              id: result.draggableId,
-              precedingId,
-            });
+            // reorder({
+            //   id: result.draggableId,
+            //   precedingId,
+            // });
           }
         }}
       >
--- src/components/ShoppingList/ShoppingListItem.tsx
+++ src/components/ShoppingList/ShoppingListItem.tsx
@@ -1,12 +1,11 @@
 import { Center } from "@mantine/core";
-import type { ShoppingListItem } from "@prisma/client";
 import { IconGripVertical } from "@tabler/icons-react";
 import { useState } from "react";
-import { trpc } from "../../utils/trpc";
 import type { ShoppingListItemValues } from "./ShoppingListItemForm";
 import { ShoppingListItemForm } from "./ShoppingListItemForm";
 import { Draggable } from "react-beautiful-dnd";
 import { NonBlockingLoader } from "../NonBlockingLoader";
+import type { ShoppingListItem } from "../../models/ShoppingListItem";
 
 export function ShoppingListExistingItem({
   item,
@@ -20,46 +19,47 @@ export function ShoppingListExistingItem({
     quantity: item.quantity,
     purchased: item.purchased,
   });
-  const utils = trpc.useContext();
 
-  const { mutate: updateItem, isLoading } =
-    trpc.shoppingList.updateItem.useMutation({
-      async onMutate(updatedItem) {
-        // Cancel outgoing fetches (so they don't overwrite our optimistic update)
-        await utils.shoppingList.getAll.cancel();
+  // const { mutate: updateItem, isLoading } =
+  //   trpc.shoppingList.updateItem.useMutation({
+  //     async onMutate(updatedItem) {
+  //       // Cancel outgoing fetches (so they don't overwrite our optimistic update)
+  //       await utils.shoppingList.getAll.cancel();
 
-        const previousData = utils.shoppingList.getAll.getData();
+  //       const previousData = utils.shoppingList.getAll.getData();
 
-        utils.shoppingList.getAll.setData(undefined, (old) =>
-          !old
-            ? old
-            : old.map((o) => {
-                if (o.id === updatedItem.id) {
-                  return {
-                    ...o,
-                    ...updatedItem,
-                  };
-                } else {
-                  return o;
-                }
-              })
-        );
+  //       utils.shoppingList.getAll.setData(undefined, (old) =>
+  //         !old
+  //           ? old
+  //           : old.map((o) => {
+  //               if (o.id === updatedItem.id) {
+  //                 return {
+  //                   ...o,
+  //                   ...updatedItem,
+  //                 };
+  //               } else {
+  //                 return o;
+  //               }
+  //             })
+  //       );
 
-        return { previousData };
-      },
+  //       return { previousData };
+  //     },
 
-      onSettled: () => {
-        utils.shoppingList.getAll.invalidate();
-      },
+  //     onSettled: () => {
+  //       utils.shoppingList.getAll.invalidate();
+  //     },
 
-      onError(err, newItem, ctx) {
-        // If the mutation fails, use the context-value from onMutate
-        utils.shoppingList.getAll.setData(
-          undefined,
-          () => ctx?.previousData ?? []
-        );
-      },
-    });
+  //     onError(err, newItem, ctx) {
+  //       // If the mutation fails, use the context-value from onMutate
+  //       utils.shoppingList.getAll.setData(
+  //         undefined,
+  //         () => ctx?.previousData ?? []
+  //       );
+  //     },
+  //   });
+
+  const isLoading = false;
 
   return (
     <>
@@ -79,10 +79,10 @@ export function ShoppingListExistingItem({
               }
               values={itemValues}
               onValuesChange={(newValues) => {
-                updateItem({
-                  id: item.id,
-                  ...newValues,
-                });
+                // updateItem({
+                //   id: item.id,
+                //   ...newValues,
+                // });
                 setItemValues(newValues);
               }}
             />
