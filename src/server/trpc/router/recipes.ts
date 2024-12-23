7a3265228dab0625c4409c39c70f3b4569e163a6
Daniel Higgins <daniel.higgins@youraspire>
2024-12-23 15:41:06 -0500

    fixes

--- src/server/trpc/router/recipes.ts
+++ src/server/trpc/router/recipes.ts
@@ -1,9 +1,7 @@
 import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
-import type { Prisma, PrismaClient } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 import { env } from "../../../env/server.mjs";
-import type { Container } from "@azure/cosmos";
-import { CosmosClient } from "@azure/cosmos";
 import cloudinary from "cloudinary";
 
 const recipeBase = z.object({
@@ -72,7 +70,7 @@ export const recipeRouter = router({
         for (let i = 0; i < originalPhotos.length; i++) {
           const originalPhoto = originalPhotos[i] as string;
           if (!input.photos || !input.photos.includes(originalPhoto)) {
-            await deleteImageIfSet(ctx.prisma, input, originalPhoto);
+            await deleteImageIfSet(originalPhoto);
           }
         }
       }
@@ -109,7 +107,7 @@ export const recipeRouter = router({
 
         for (let i = 0; i < originalPhotos.length; i++) {
           const originalPhoto = originalPhotos[i] as string;
-          await deleteImageIfSet(ctx.prisma, input, originalPhoto);
+          await deleteImageIfSet(originalPhoto);
         }
       }
 
@@ -121,25 +119,6 @@ export const recipeRouter = router({
         },
       });
     }),
-
-  importRecipes: publicProcedure.mutation(async ({ ctx }) => {
-    const originalRecipes = await getRecipes();
-    for (let i = 0; i < originalRecipes.length; i++) {
-      const originalRecipe = originalRecipes[i];
-      if (!!originalRecipe) {
-        await ctx.prisma.recipe.create({
-          data: {
-            title: originalRecipe.title,
-            notes: originalRecipe.notes,
-            ingredients: originalRecipe.ingredients?.map((i) => i.name) ?? [],
-            photos: originalRecipe.files?.map((f) => f.url) ?? [],
-            tags: originalRecipe.tags?.map((t) => t.name) ?? [],
-            link: originalRecipe.link,
-          },
-        });
-      }
-    }
-  }),
 
   addToShoppingList: publicProcedure
     .input(
@@ -204,13 +183,7 @@ export const recipeRouter = router({
     }),
 });
 
-async function deleteImageIfSet(
-  prismaClient: PrismaClient<Prisma.PrismaClientOptions, never>,
-  input: {
-    id: string;
-  },
-  photo: string,
-) {
+async function deleteImageIfSet(photo: string) {
   if (photo) {
     cloudinary.v2.config({
       cloud_name: env.CLOUDINARY_CLOUD_NAME,
@@ -220,47 +193,4 @@ async function deleteImageIfSet(
 
     await cloudinary.v2.uploader.destroy(photo);
   }
-}
-
-let container: Container | null = null;
-async function getContainer() {
-  if (container) {
-    return container;
-  }
-
-  const client = new CosmosClient(
-    "AccountEndpoint=https://restauranttracker.documents.azure.com:443/;AccountKey=82ZvKAIecGTIR9dE0V19rAirHcJmhG9UheCDWyshfO61CCSaIkDVnxZwII8Ovys3qhnijMm8BVxjaV2r9QIfWw==;",
-  );
-  const { database } = await client.databases.createIfNotExists({
-    id: "RestaurantTracker",
-  });
-  const r = await database.containers.createIfNotExists({
-    id: "Recipes",
-  });
-  container = r.container;
-
-  return container;
-}
-
-export type CosmosBase = {
-  type: string;
-};
-
-export type CosmosRecipe = {
-  title: string;
-  notes: string;
-  ingredients?: Array<{ id: string; name: string }>;
-  files?: Array<{ url: string; id: string }>;
-  tags?: Array<{ name: string }>;
-  link?: string;
-} & CosmosBase;
-
-async function getRecipes() {
-  const container = await getContainer();
-
-  const { resources } = await container.items
-    .query("SELECT * FROM c WHERE c.type = 'recipe'")
-    .fetchAll();
-
-  return resources as Array<CosmosRecipe>;
 }
